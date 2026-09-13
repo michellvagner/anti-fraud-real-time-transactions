28fa116f51542dec23564c34da8df20bf04d1b37
Vagner Michell <vagner.michell@gmail.com>
2026-09-12 23:43:39 -0300

    test: restore location assertion

--- tests/test_location.py
+++ tests/test_location.py
@@ -4,6 +4,6 @@ from src.gerador_transacoes import generate_location
 def test_generate_location():
     location = generate_location()
 
-    assert location["country"] is None 
+    assert location["country"] is not None 
     assert location["country_cd"] is not None 
     assert location["currency"] is not None
